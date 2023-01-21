51995ab683854514f0b3cd58e820784e16b15474
FreeHi <freehi@cumallover.me>
2023-01-21 14:27:08 +0800

    Remove unused libraries

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp, reactive, ref, Vue } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue';
 import router from './router';
 import { IonicVue } from '@ionic/vue';
