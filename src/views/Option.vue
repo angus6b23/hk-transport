e2aade46607334a25843f7ad84cf466e539e227c
Angus Wan <work@12a.app>
2023-05-26 14:17:07 +0800

    Added basic reactivity to options

--- src/views/Option.vue
+++ src/views/Option.vue
@@ -16,8 +16,17 @@
 		<ion-list>
 			<ion-item button>
 				<ion-label>
+					<h5>語言 / Language</h5>
+					<p v-if="config.lang == 'zh'">正體中文</p>
+					<p v-else-if="config.lang == 'en'">English</p>
+				</ion-label>
+			</ion-item>
+			<ion-item button>
+				<ion-label>
 					<h5>資料來源</h5>
-					<p>預設</p>
+					<p v-if="config.fetchMethod == 'default'">預設</p>
+					<p v-else-if="config.fetchMethod == 'hkgov'">來自 gov.hk</p>
+					<p v-else-if="config.fetchMethod == 'self'">自行搭建來源: {{ config.apiBaseUrl }}</p>
 				</ion-label>
 			</ion-item>
 		</ion-list>
@@ -25,14 +34,14 @@
 			<ion-label>資料管理</ion-label>
 		</ion-list-header>
 		<ion-list>
-			<ion-item button>
+			<ion-item button @click="clearStarred">
 				<ion-label>
 					<h5>清除所有已標記的路線</h5>
 				</ion-label>
 			</ion-item>
 			<ion-item button @click="confirmClearData">
 				<ion-label>
-					<h5>清除所有資料</h5>
+					<h5>重設所有設定</h5>
 				</ion-label>
 			</ion-item>
 		</ion-list>
@@ -52,27 +61,40 @@ export default {
 	components: { IonHeader, IonTitle, IonContent, IonList, IonListHeader, IonLabel, IonItem,  IonIcon, IonButton, IonButtons, IonSegment, IonSegmentButton, IonToolbar },
 	emits: ['closeOption'],
 	setup() {
+		const config = ref({})
 		return {
-			chevronBack
+			chevronBack,
+			config
 		}
 	},
 	async mounted() {
+		this.config = await localforage.getItem('config')
 	},
 	methods: {
 		closeOption() {
 			this.$emit('closeOption');
 		},
+		async clearStarred() {
+			const {value} = await Dialog.confirm({
+				title: '清除所有已標記的路線',
+				message: '你確定要清除所有已標記的路線嗎？'
+			});
+			if (value){
+				await localforage.setItem('starred', []);
+				presentToast('info', '已清除所有已標記的路線')
+			}
+		},
 		async confirmClearData() {
 			const { value } = await Dialog.confirm({
-				title: '清除所有資料',
-				message: '所有路線資料需再次下載，已標記的路線會被清除。\n你確定要清除所有資料嗎？'
+				title: '重設所有資料',
+				message: '所有路線資料需再次下載，已標記的路線亦會被清除。\n你確定要重設所有資料嗎？'
 			});
 			if (value){
 				let keys = await localforage.keys();
 				for (let key of keys){
 					await localforage.removeItem(key)
 				}
-				presentToast('info', '資料已被清除，將會在3秒後重新載入');
+				presentToast('info', '已重設所有設定，將會在3秒後重新載入');
 				setTimeout(()=>{ location.reload() }, 3000)
 			}
 		}
