5805775c6abd358fc14e70baa2f14fb661067b6f
Angus Wan <angus6b23@gmail.com>
2023-08-26 20:10:05 +0100

    Added translation to map and routeinfo

--- src/components/RouteInfo.vue
+++ src/components/RouteInfo.vue
@@ -3,15 +3,15 @@
         <ion-item>
             <ion-grid>
                 <ion-row>
-                    <ion-col size="6" class="ion-text-left">車程</ion-col>
-                    <ion-col size="6" class="ion-text-right">{{ item.journeyTime }} 分鐘</ion-col>
+                    <ion-col size="6" class="ion-text-left">{{ $t('etaPopup.info.journeyTime') }}</ion-col>
+                    <ion-col size="6" class="ion-text-right">{{ `${item.journeyTime} ${$t('common.minute')}` }} </ion-col>
                 </ion-row>
             </ion-grid>
         </ion-item>
         <ion-item>
             <ion-grid>
                 <ion-row>
-                    <ion-col size="6" class="ion-text-left">全程收費</ion-col>
+                    <ion-col size="6" class="ion-text-left">{{ $t('etaPopup.info.fullFare') }}</ion-col>
                     <ion-col size="6" class="ion-text-right">${{ item.fullFare }}</ion-col>
                 </ion-row>
             </ion-grid>
@@ -19,7 +19,12 @@
         <ion-item :href="item.infoLinkTC" target="_blank" button>
             <ion-grid>
                 <ion-row>
-                    <ion-col size="6" class="ion-text-left">路線詳情</ion-col>
+                    <ion-col size="6" class="ion-text-left">
+                        {{ $t('etaPopup.info.details') }}
+                    </ion-col>
+                    <ion-col size="6" class="ion-text-right">
+                        <ion-icon :icon="openOutline"></ion-icon>
+                    </ion-col>
                 </ion-row>
             </ion-grid>
         </ion-item>
@@ -27,10 +32,10 @@
             <ion-grid>
                 <ion-row v-for="(section, index) in item.timetable" :key="index">
                     <ion-card class="timetable-card ion-padding">
-                        <ion-card-title v-if="section.title == 'MF'">星期一至五</ion-card-title>
-                        <ion-card-title v-else-if="section.title == 'S'">星期六</ion-card-title>
-                        <ion-card-title v-else-if="section.title == 'H'">星期日及公眾假期</ion-card-title>
-                        <ion-card-title v-else-if="section.title == 'D'">每日</ion-card-title>
+                        <ion-card-title v-if="section.title == 'MF'">{{ $t('etaPopup.info.MF') }}</ion-card-title>
+                        <ion-card-title v-else-if="section.title == 'S'">{{ $t('etaPopup.info.S') }}</ion-card-title>
+                        <ion-card-title v-else-if="section.title == 'H'">{{ $t('etaPopup.info.H') }}</ion-card-title>
+                        <ion-card-title v-else-if="section.title == 'D'">{{ $t('etaPopup.info.D') }}</ion-card-title>
                         <ion-card-content>
                             <ion-grid>
                                 <ion-row v-for="(detail, index) in section.details" :key="`detail-${index}`">
@@ -40,7 +45,7 @@
                                     <ion-col size="6" class="ion-text-right" v-if="detail.freq">
                                         <ion-text>{{ detail.freq }}</ion-text>
                                     </ion-col>
-    
+
                                 </ion-row>
 
                             </ion-grid>
@@ -54,15 +59,17 @@
 
 <script>
 import { ref, defineComponent } from 'vue';
-import { IonList, IonItem, IonGrid, IonRow, IonCol, IonText, IonCard, IonCardTitle, IonCardContent } from '@ionic/vue';
+import { IonList, IonItem, IonGrid, IonRow, IonCol, IonText, IonCard, IonCardTitle, IonCardContent, IonIcon } from '@ionic/vue';
+import { openOutline } from 'ionicons/icons'
 export default defineComponent({
     name: "RouteInfo",
-    components: { IonList, IonItem, IonGrid, IonRow, IonCol, IonText, IonCard, IonCardTitle, IonCardContent },
+    components: { IonList, IonItem, IonGrid, IonRow, IonCol, IonText, IonCard, IonCardTitle, IonCardContent, IonIcon },
     props: ['item'],
     setup(props){
         const item = ref(props.item);
         return{
-            item
+            item,
+            openOutline
         }
     }
 });
